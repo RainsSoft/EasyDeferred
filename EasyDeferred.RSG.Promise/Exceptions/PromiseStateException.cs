de3081650dbfa163a94b3f64c3fa536e2f60b381
andyhebear <andyhebear@163.com>
2022-10-18 14:32:04 +0800

    add RSG Promise

---
+++ EasyDeferred.RSG.Promise/Exceptions/PromiseStateException.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using EasyDeferred.RSG.Linq;
+using System.Text;
+
+namespace EasyDeferred.RSG.Exceptions
+{
+    /// <summary>
+    /// Exception thrown when an operation is performed on a promise that is in an invalid
+    /// state for it to handle.
+    /// </summary>
+    public class PromiseStateException : PromiseException
+    {
+        public PromiseStateException() { }
+
+        public PromiseStateException(string message) : base(message) { }
+
+        public PromiseStateException(string message, Exception inner) 
+            : base(message, inner)
+        { }
+    }
+}
