f0c25d6997a7abc4ba91d064c9b965882911ca77
andyhebear <andyhebear@163.com>
2022-11-04 19:06:58 +0800

    Create InvokeCompletedEventArgs.cs

---
+++ EasyDeferred.Synchronize/InvokeCompletedEventArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+
+
+namespace EasyDeferred.Synchronize
+{
+    /// <summary>
+    ///     Represents information about the InvokeCompleted event.
+    /// </summary>
+    public class InvokeCompletedEventArgs : AsyncCompletedEventArgs
+    {
+        private readonly object[] args;
+
+        public InvokeCompletedEventArgs(Delegate method, object[] args, object result, Exception error)
+            : base(error, false, null) {
+            Method = method;
+            this.args = args;
+            Result = result;
+        }
+
+        public Delegate Method { get; }
+
+        public object Result { get; }
+
+        public object[] GetArgs() {
+            return args;
+        }
+    }
+}
