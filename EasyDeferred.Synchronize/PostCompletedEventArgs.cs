b97fe55252457b3951972bf56bb0f37b060bc56b
andyhebear <andyhebear@163.com>
2022-11-07 20:13:57 +0800

    Create PostCompletedEventArgs.cs

---
+++ EasyDeferred.Synchronize/PostCompletedEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+
+namespace EasyDeferred.Synchronize
+{
+    public class PostCompletedEventArgs : AsyncCompletedEventArgs
+    {
+        public PostCompletedEventArgs(SendOrPostCallback callback, Exception error, object state)
+            : base(error, false, state) {
+            Callback = callback;
+        }
+
+        public SendOrPostCallback Callback { get; }
+    }
+}
