b93ebc18ff490d43ebc5d8c8cf657aff5ad7d37c
andyhebear <andyhebear@163.com>
2022-12-08 20:17:19 +0800

    Create EasyDeferredSyncContextInvoke.cs

---
+++ EasyDeferredSyncContextInvoke.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading;
+
+namespace EasyDeferred
+{
+    public class EasySynchronizationContextInvokeBase : SynchronizationContext, ISynchronizeInvoke
+    {
+        public virtual bool InvokeRequired => throw new NotImplementedException();
+
+        public virtual IAsyncResult BeginInvoke(Delegate method,params object[] args) {
+            throw new NotImplementedException();
+        }
+
+        public virtual object EndInvoke(IAsyncResult result) {
+            throw new NotImplementedException();
+        }
+
+        public virtual object Invoke(Delegate method, params object[] args) {
+            throw new NotImplementedException();
+        }
+    }
+}
