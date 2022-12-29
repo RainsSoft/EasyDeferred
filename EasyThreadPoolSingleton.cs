cc53f4d3ff6d40d5ad07dcdb8b5dd34199eaaf22
andyhebear <andyhebear@163.com>
2022-12-29 20:58:23 +0800

    Create EasyThreadPoolSingleton.cs

---
+++ EasyThreadPoolSingleton.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using EasyDeferred.Core;
+using EasyDeferred.ThreadPooling;
+namespace EasyDeferred
+{
+    public class EasyThreadPoolSingleton : Singleton<EasyThreadPoolSingleton>
+    {
+        private EasyThreadPool m_ThreadPool;
+        public EasyThreadPool ThreadPool { get { return m_ThreadPool; } }
+        public override bool Initialize(params object[] args) {
+            if (m_ThreadPool == null) {
+                int threadNum = Math.Max(Environment.ProcessorCount, (int)((2 % Environment.ProcessorCount) + 1) * Environment.ProcessorCount / 2);
+                m_ThreadPool = new EasyThreadPool("easy",threadNum);                
+            }
+            return base.Initialize(args);
+        }
+        protected override void dispose(bool disposeManagedResources) {
+            if (m_ThreadPool != null) {
+                m_ThreadPool.Dispose();
+            }
+            m_ThreadPool = null;
+            base.dispose(disposeManagedResources);
+        }
+        public bool QueueWorkItem(WaitCallback callBack) {
+
+            return false;
+        }
+        public bool QueueWorkItem(WaitCallback callBack, object state) {
+
+            return false;
+        }
+    }
+    public class FairThreadPoolSingleton : Singleton<FairThreadPoolSingleton>
+    {
+
+        private FairThreadPool.FairThreadPool m_ThreadPool;
+        public FairThreadPool.FairThreadPool ThreadPool { get { return m_ThreadPool; } }
+        public override bool Initialize(params object[] args) {
+            if (m_ThreadPool == null) {
+                int threadNum= Math.Max(Environment.ProcessorCount,(int)((2 % Environment.ProcessorCount) + 1) * Environment.ProcessorCount/2);
+                m_ThreadPool = new FairThreadPool.FairThreadPool("fair",threadNum);
+            }
+            return base.Initialize(args);
+        }
+        protected override void dispose(bool disposeManagedResources) {
+            if (m_ThreadPool != null) {
+                m_ThreadPool.Dispose(); 
+            }
+            m_ThreadPool = null;
+            base.dispose(disposeManagedResources);
+        }
+    }
+    public class SimpleThreadPoolSingleton : Singleton<SimpleThreadPoolSingleton>
+    {
+
+        private SimpleThreadPool.SimpleThreadPool m_ThreadPool;
+        public SimpleThreadPool.SimpleThreadPool ThreadPool { get { return m_ThreadPool; } }
+        public override bool Initialize(params object[] args) {
+            if (m_ThreadPool == null) {
+                int threadNum = Math.Max(Environment.ProcessorCount, (int)((2 % Environment.ProcessorCount) + 1) * Environment.ProcessorCount / 2);
+                m_ThreadPool = new SimpleThreadPool.SimpleThreadPool("simple",1,threadNum,1);
+            }
+            return base.Initialize(args);
+        }
+        protected override void dispose(bool disposeManagedResources) {
+            if (m_ThreadPool != null) {
+                m_ThreadPool.Dispose();
+            }
+            m_ThreadPool = null;
+            base.dispose(disposeManagedResources);
+        }
+    }
+
+}
